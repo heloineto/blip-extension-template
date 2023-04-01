681662f5b72e057b245f1c6465da9400198a55cb
Heloi Neto <hey@heloineto.com>
2023-03-31 22:55:04 -0300

    feat: add code coverage

--- cypress/support/component.ts
+++ cypress/support/component.ts
@@ -1,6 +1,7 @@
 import '@/index.css';
 import '@/lib/setup/blip-ds';
 import '@/lib/setup/fonts';
+import '@cypress/code-coverage/support';
 import { mount } from 'cypress/react18';
 import '../../src/@types/cypress.d.ts';
 import './commands';
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import react from '@vitejs/plugin-react-swc';
 import path from 'node:path';
 import { defineConfig } from 'vite';
+import istanbul from 'vite-plugin-istanbul';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), istanbul({ cypress: true })],
     optimizeDeps: { exclude: ['blip-ds/loader'] },
     server: { port: 3000 },
     resolve: {
