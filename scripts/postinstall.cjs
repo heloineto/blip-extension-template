b9910d943c8a37e9bf95cf178f3d053311977f3f
Heloi Neto <hey@heloineto.com>
2023-04-22 09:08:57 -0300

    feat: change package name on postinstall

--- scripts/postinstall.cjs
+++ scripts/postinstall.cjs
@@ -1,38 +1,33 @@
 const fs = require('node:fs');
+const path = require('node:path');
 const prodSettings = require('../src/config/appsettings.json');
 
 const DEV_SETTINGS_PATH = './src/config/appsettings.development.json';
 const PROD_SETTINGS_PATH = './src/config/appsettings.json';
 const INITIAL_PREFIX = 'blip-extension-template';
+const INITIAL_PACKAGE_NAME = 'blip-extension-template';
 
-const changePrefix = () => {
+const changePrefix = (prefix) => {
     const currentPrefix = prodSettings.segment.prefix;
-    const packageName = process.env.npm_package_name;
 
-    if (currentPrefix === INITIAL_PREFIX) {
-        const newProdSettings = {
-            ...prodSettings,
-            segment: { ...prodSettings.segment, prefix: packageName },
-        };
-
-        fs.writeFileSync(
-            PROD_SETTINGS_PATH,
-            JSON.stringify(newProdSettings, null, 4)
-        );
-
-        return packageName;
+    if (currentPrefix !== INITIAL_PREFIX) {
+        return currentPrefix;
     }
 
-    return currentPrefix;
+    const newProdSettings = {
+        ...prodSettings,
+        segment: { ...prodSettings.segment, prefix },
+    };
+
+    fs.writeFileSync(
+        PROD_SETTINGS_PATH,
+        JSON.stringify(newProdSettings, null, 4)
+    );
+
+    return prefix;
 };
 
-const main = () => {
-    if (fs.existsSync(DEV_SETTINGS_PATH)) {
-        return;
-    }
-
-    const prefix = changePrefix();
-
+const createDevSettings = (prefix) => {
     const devSettings = {
         ...prodSettings,
         segment: { ...prodSettings.segment, prefix },
@@ -40,6 +35,37 @@ const main = () => {
     };
 
     fs.writeFileSync(DEV_SETTINGS_PATH, JSON.stringify(devSettings, null, 4));
+};
+
+const changePackageName = (name) => {
+    const packageJson = require('../package.json');
+
+    if (packageJson.name !== INITIAL_PACKAGE_NAME) {
+        return packageJson.name;
+    }
+
+    fs.writeFileSync(
+        './package.json',
+        JSON.stringify({ ...packageJson, name }, null, 4)
+    );
+
+    return name;
+};
+
+const main = () => {
+    if (fs.existsSync(DEV_SETTINGS_PATH)) {
+        return;
+    }
+
+    const folderName = path.basename(path.resolve(process.cwd()));
+    const packageName = changePackageName(folderName);
+
+    const newPrefix = packageName.startsWith('blip-')
+        ? packageName.replace('blip-', '')
+        : packageName;
+
+    const prefix = changePrefix(newPrefix);
+    createDevSettings(prefix);
 };
 
 main();
