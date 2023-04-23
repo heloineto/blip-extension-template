58426cd8092591ff9343aca4c6d2675c2468d4c3
Heloi Neto <hey@heloineto.com>
2023-04-23 13:49:14 -0300

    feat: organize postinstall script

---
+++ scripts/postinstall/changePrefix.cjs
@@ -0,0 +1,27 @@
+const fs = require('node:fs');
+const prodSettings = require('../src/config/appsettings.json');
+
+const PROD_SETTINGS_PATH = './src/config/appsettings.json';
+const INITIAL_PREFIX = 'blip-extension-template';
+
+const changePrefix = (prefix) => {
+    const currentPrefix = prodSettings.segment.prefix;
+
+    if (currentPrefix !== INITIAL_PREFIX) {
+        return currentPrefix;
+    }
+
+    const newProdSettings = {
+        ...prodSettings,
+        segment: { ...prodSettings.segment, prefix },
+    };
+
+    fs.writeFileSync(
+        PROD_SETTINGS_PATH,
+        JSON.stringify(newProdSettings, null, 4)
+    );
+
+    return prefix;
+};
+
+export default changePrefix;
