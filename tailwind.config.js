9fd6f4f074722e9add4cbb56d34f2bf5845e6037
Heloi Neto <hey@heloineto.com>
2023-07-21 21:45:18 -0300

    chore: update dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    content: ['./public/index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
         extend: {
             fontFamily: {
