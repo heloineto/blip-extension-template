8d50d7fc2158b9c9bba284879a871fb60ff98692
Heloi Neto <hey@heloineto.com>
2024-03-21 11:24:44 -0300

    feat: hard update dependencies

--- src/@types/i18n.d.ts
+++ src/@types/i18n.d.ts
@@ -1,14 +1,9 @@
-import enTranslation from '@/assets/locales/en/translation.json';
-import esTranslation from '@/assets/locales/es/translation.json';
-import ptTranslation from '@/assets/locales/pt/translation.json';
+import enTranslation from '../assets/locales/en/translation.json';
 
 declare module 'i18next' {
   interface CustomTypeOptions {
     resources: {
-      translations:
-        | typeof enTranslation
-        | typeof esTranslation
-        | typeof ptTranslation;
+      translations: typeof enTranslation;
     };
   }
 }
