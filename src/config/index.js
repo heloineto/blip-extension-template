78552eb8adfa5e70ad09ac38791c0f988622b71e
Heloi Neto <hey@heloineto.com>
2024-03-22 13:54:39 -0300

    fix: change how configuration is exported

--- src/config/index.js
+++ src/config/index.js
@@ -1,12 +1,13 @@
 import isDev from '../lib/utils/isDev';
 import productionConfig from './appsettings.json';
 
-let config;
-
-try {
-  config = isDev ? require('./appsettings.development.json') : productionConfig;
-} catch (error) {
-  config = productionConfig;
+function getConfig() {
+  try {
+    return isDev ? require('./appsettings.development.json') : productionConfig;
+  } catch (error) {
+    return productionConfig;
+  }
 }
 
+const config = getConfig();
 export default config;
